b46e73d5ffebfb441ba7bb6064a8060b1834ee59
unknown <kevin123488@naver.com>
2024-08-08 22:57:21 +0900

    0808 algo

--- 2024/8월/0804/17404.py
+++ 2024/8월/0804/17404.py
@@ -1,6 +1,56 @@
 import sys
 sys.stdin = open('17404.txt')
+sys.setrecursionlimit(100000)
+
+def dfs(n, now_house_idx, now_color, first_color, now_spent):
+    # 집의 수, 현재 탐색 완료한 집의 인덱스, 방금 탐색한 집에서 선택한 색상, 첫번째로 선택한 색상, 지금까지 소비한 금액
+    global ans
+    
+    if now_spent > ans: # 유망하지 않은 루트
+        return
+
+    if now_house_idx == -1: # 아직 어떤 집도 탐색하지 않음
+        if ans > house[0][0]:
+            dfs(n, 0, 0, 0, house[0][0])
+        if ans > house[0][1]:
+            dfs(n, 0, 1, 1, house[0][1])
+        if ans > house[0][2]:
+            dfs(n, 0, 2, 2, house[0][2])
+
+    elif 0 <= now_house_idx < n - 2:
+        for color in range(3):
+            if color != now_color and now_spent + house[now_house_idx + 1][color] < ans:
+                dfs(n, now_house_idx + 1, color, first_color, now_spent + house[now_house_idx + 1][color])
+    
+    elif now_house_idx == n - 2: # 마지막 집까지 모두 탐색했을 때
+        for color in range(3):
+            if color != now_color and color != first_color and now_spent + house[now_house_idx + 1][color] < ans:
+                dfs(n, now_house_idx + 1, color, first_color, now_spent + house[now_house_idx + 1][color])
+        
+    else: # 마지막 집까지 모두 탐색했을 때
+        if ans > now_spent:
+            ans = now_spent
+            return
 
 n = int(input())
-house = [list(map(int, input().split())) for _ in range(n)]
-# print(n, house)
+house = [list(map(int, input().split())) for _ in range(n)] # 각 집을 빨, 초, 파로 칠하는 비용
+
+# 1번 집의 색은 2번, N번 집의 색과 같지 않아야 한다.
+# N번 집의 색은 N-1번, 1번 집의 색과 같지 않아야 한다.
+# i(2 ≤ i ≤ N-1)번 집의 색은 i-1, i+1번 집의 색과 같지 않아야 한다.
+
+# 요약
+# 연속된 집의 경우 색이 달라야 함
+# 1번과 n번의 집의 색은 달라야 함
+
+# 가능한 모든 케이스를 확인한 후 비용의 합을 비교하여 최솟값을 리턴하자
+ans_list = []
+
+# dfs 및 백트래킹 사용
+visited = [-1] * n
+ans = 1000009
+# dfs를 수행할 때 첫번째 집의 색과 직전에 선택한 색의 정보를 넘겨주자
+dfs(n, -1, -1, -1, 0) # 색은 0 -> 빨, 1 -> 초, 2 -> 파.
+
+# print(visited)
+print(ans)
---
+++ 2024/8월/0804/dp_17404.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('17404.txt')
+
+n = int(input())
+house = [list(map(int, input().split())) for _ in range(n)] # 각 집을 빨, 초, 파로 칠하는 비용
+
+# dp로 해결
+# 집 개수만큼 칸을 만들자. 각 칸에는 3개의 칸을 만들어두자. 각각 빨, 초, 파 임
+# dp[i - 1][0] -> i번째 집이 빨간색으로 칠해졌을 때 최소비용
+
+# 시작값 고정하고 만들어보자
+ans = 1000009
+
+for first_color in range(3): # 0 -> 빨, 1 -> 초, 2 -> 파
+    dp = [[1000009] * 3 for _ in range(n)]
+    dp[0][first_color] = house[0][first_color]
+
+    for i in range(1, n): # 집 순회
+        dp[i][0] = min(dp[i - 1][1] + house[i][0], dp[i - 1][2] + house[i][0])
+        dp[i][1] = min(dp[i - 1][0] + house[i][1], dp[i - 1][2] + house[i][1])
+        dp[i][2] = min(dp[i - 1][0] + house[i][2], dp[i - 1][1] + house[i][2])
+    
+    for last_color in range(3):
+        if last_color != first_color:
+            ans = min(ans, dp[-1][last_color])
+
+print(ans)
